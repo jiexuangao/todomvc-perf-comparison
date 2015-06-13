ea8fff209b78a61bb5744d1cfa2a996fe4ec00c7
jiexuangao <jiexuangao@gmail.com>
2015-06-13 10:10:19 +0800

    for a commonly badly used case for test

--- todomvc/rosetta/element/r-todoapp.js
+++ todomvc/rosetta/element/r-todoapp.js
@@ -1,7 +1,15 @@
 Rosetta.register('r-todoapp', function (tag) {
     var __m = arguments[0];
     __m.__t = function tmpl(tag, attrs, refs) {
-        return tag.create('div', { 'class': tag.type }, tag.create('ul', { 'id': 'todo-list' }, (attrs.list || []).map(function (item, index) {
+        return tag.create('div', { 'class': tag.type }, tag.create('section', { 'id': 'todoapp' }, tag.create('header', { 'id': 'header' }, tag.create('h1', null, 'todos'), tag.create('input', {
+            'id': 'new-todo',
+            'placeholder': 'What needs to be done?',
+            'autofocus': true,
+            'onKeyPress': submitChange
+        })), tag.create('section', { 'id': 'main' }, tag.create('input', {
+            'id': 'toggle-all',
+            'type': 'checkbox'
+        }), tag.create('label', { 'for': 'toggle-all' }, 'Mark all as complete'), tag.create('ul', { 'id': 'todo-list' }, (attrs.list || []).map(function (item, index) {
             var arr = [];
             var completed = '';
             if (item.completed) {
@@ -17,7 +25,10 @@ Rosetta.register('r-todoapp', function (tag) {
                 'onClick': deleteLi
             }))));
             return arr;
-        })));
+        }))), tag.create('footer', { 'id': 'footer' }, tag.create('span', { 'id': 'todo-count' }, tag.create('strong', null)), tag.create('ul', { 'id': 'filters' }, tag.create('li', null, tag.create('a', {
+            'class': 'selected',
+            'href': '#/'
+        })), tag.create('li', null, tag.create('a', { 'href': '#/active' })), tag.create('li', null, tag.create('a', { 'href': '#/completed' }))))));
     };
     function toggle(e) {
         var $el = $(e.target);
@@ -34,5 +45,17 @@ Rosetta.register('r-todoapp', function (tag) {
         var list = tag.attrs.list;
         list.splice(index, 1);
         tag.update({ list: list || [] });
+    }
+    function submitChange(e) {
+        var $newtodo = $(e.target);
+        if (e.which === 13 && $newtodo.val()) {
+            var tag = Rosetta.ref('aaa');
+            tag.attrs.list.push({
+                completed: '',
+                title: $newtodo.val().trim()
+            });
+            tag.update({ list: tag.attrs.list });
+            $newtodo.val('');
+        }
     }
 });
